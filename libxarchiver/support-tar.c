0580f2af956ba7c6ef48961f716e093f5aa29b65
Giuseppe Torelli <colossus73@gmail.com>
2006-03-27 07:24:31 +0000

    Corrected xarchiver.desktop file
    (Old svn revision: 20593)

--- libxarchiver/support-tar.c
+++ libxarchiver/support-tar.c
@@ -1,6 +1,6 @@
 /*
  *  Copyright (c) 2006 Stephan Arts      <stephan.arts@hva.nl>
- *                     Guiseppe Torelli
+ *                     Giuseppe Torelli <colossus73@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
