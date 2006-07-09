1f0b04a5a5b2576ad36118d54115ff8b202ecf54
Benedikt Meurer <benny@xfce.org>
2006-07-09 12:44:52 +0000

    Update build framework, as requested by Giuseppe.
    (Old svn revision: 22312)

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# $Id: autogen.sh 8094 2005-01-27 07:52:08Z kelnos $
+# $Id$
 #
-# Copyright (c) 2002-2005
+# Copyright (c) 2002-2006
 #         The Xfce development team. All rights reserved.
 #
 # Written for Xfce by Benedikt Meurer <benny@xfce.org>.
@@ -17,6 +17,22 @@ autogen.sh: You don't seem to have the Xfce development tools installed on
 EOF
   exit 1
 }
+
+# verify that po/LINGUAS is present
+(test -f po/LINGUAS) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
+            or try to checkout again.
+EOF
+  exit 1
+}
+
+# substitute revision and linguas
+linguas=`sed -e '/^#/d' po/LINGUAS`
+revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}'`
+sed -e "s/@LINGUAS@/${linguas}/g" \
+    -e "s/@REVISION@/${revision}/g" \
+    < "configure.in.in" > "configure.in"
 
 exec xdt-autogen $@
 
