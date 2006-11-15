72cfe4d2f8dbe09202587f829c778cac06a7e5b8
Giuseppe Torelli <colossus73@gmail.com>
2006-11-15 13:13:58 +0000

    Changed the add and extract buttons with those from the Tango icon set. Moved some lines regarding g_free() in support.c to avoid memory leaks.
    (Old svn revision: 23887)

--- src/support.c
+++ src/support.c
@@ -30,28 +30,30 @@ GtkWidget *xa_main_window_find_image(gchar *filename, GtkIconSize size)
 	GError *error = NULL;
     GtkWidget *file_image;
 	gchar *path;
-	path = g_strconcat(DATADIR, "/xarchiver/pixmaps/", filename, NULL);
-	GdkPixbuf *file_pixbuf = gdk_pixbuf_new_from_file(path, &error);
-	if(!file_pixbuf)
+	GdkPixbuf *file_pixbuf = NULL;
+
+	path = g_strconcat("/usr/share/", "/xarchiver/pixmaps/", filename, NULL);
+	file_pixbuf = gdk_pixbuf_new_from_file (path, &error);
+	g_free (path);
+	if ( file_pixbuf == NULL )
 	{
 		/* perhaps xarchiver has not been installed and is being executed from source dir */
 		g_free (error);
-		g_free (path);
 		error = NULL;
 		path = g_strconcat("./pixmaps/", filename, NULL);
-		file_pixbuf = gdk_pixbuf_new_from_file(path, &error);
+		file_pixbuf = gdk_pixbuf_new_from_file (path, &error);
+		g_free(path);
     }
     if (file_pixbuf)
     {
-		file_image = gtk_image_new_from_pixbuf(file_pixbuf);
-		g_object_unref(file_pixbuf);
+		file_image = gtk_image_new_from_pixbuf (file_pixbuf);
+		g_object_unref (file_pixbuf);
     }
     else
 	{
 		g_free(error);
 		file_image = gtk_image_new_from_stock(GTK_STOCK_MISSING_IMAGE, size);
     }
-    g_free(path);
     return file_image;
 }
 
