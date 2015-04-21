3937c783a495b77ca2cc259a5c03b3dbb1a647fd
Ingo Brückl <ib@wupperonline.de>
2015-04-21 09:42:47 +0200

    Use IANA registered application/gzip in addition to former x-gzip

--- src/mime.c
+++ src/mime.c
@@ -48,7 +48,7 @@ const char *xa_get_stock_mime_icon(char *filename)
 		|| strcmp(mime,"application/x-7z-compressed") == 0 || strcmp(mime,"application/x-bzip-compressed-tar") == 0
 		|| strcmp (mime,"application/x-compressed-tar") == 0 || strcmp (mime,"application/x-xz-compressed-tar") == 0 || strcmp (mime,"application/x-lha") == 0
 		|| strcmp (mime,"application/x-rpm") == 0 || strcmp (mime,"application/x-deb") == 0
-		|| strcmp (mime,"application/x-bzip") == 0  || strcmp (mime,"application/x-gzip") == 0
+		|| strcmp (mime,"application/x-bzip") == 0  || strcmp (mime,"application/x-gzip") == 0 || strcmp (mime,"application/gzip") == 0
 		|| strcmp (mime,"application/x-xz") == 0)
 		icon_name = "package";
 	else if (strcmp(mime,"application/x-shockwave-flash") == 0 || strcmp(mime,"video/mpeg") == 0 || strcmp(mime,"video/quicktime") == 0
