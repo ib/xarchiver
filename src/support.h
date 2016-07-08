3108a5cadc350dd331eff16c509cfc8fed4783c8
Ingo Brückl <ib@wupperonline.de>
2016-07-08 16:28:00 +0200

    Clean up support.h
    Remove unused stuff and rearrange definitions.

--- src/support.h
+++ src/support.h
@@ -27,23 +27,14 @@
  */
 #ifdef ENABLE_NLS
 #  include <libintl.h>
-#  undef _
+#  define gettext_noop(String) String
 #  define _(String) dgettext (PACKAGE, String)
-#  define Q_(String) g_strip_context ((String), gettext (String))
-#  ifdef gettext_noop
 #    define N_(String) gettext_noop (String)
-#  else
-#    define N_(String) (String)
-#  endif
+#  define Q_(String) g_strip_context ((String), gettext (String))
 #else
-#  define textdomain(String) (String)
-#  define gettext(String) (String)
-#  define dgettext(Domain,Message) (Message)
-#  define dcgettext(Domain,Message,Type) (Message)
-#  define bindtextdomain(Domain,Directory) (Domain)
 #  define _(String) (String)
-#  define Q_(String) g_strip_context ((String), (String))
 #  define N_(String) (String)
+#  define Q_(String) g_strip_context ((String), (String))
 #endif
 
 
