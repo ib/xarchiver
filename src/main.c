115f0990c83e35893f0a1afa276dfaea48a9a52b
Ingo Brückl <ib@wupperonline.de>
2017-05-24 11:43:00 +0200

    Utilize 7zip as a substitute for gzip

--- src/main.c
+++ src/main.c
@@ -256,17 +256,26 @@ static void xa_check_available_archivers ()
 	else
 		path = g_find_program_in_path("gunzip");
 
+	standard = (path != NULL);
+
+	if (!standard && is7za)
+	{
+		/* alternative compressor */
+		path = g_strconcat(sevenz, " -tgzip", NULL);
+		archiver[type].is_compressor = TRUE;
+	}
+
 	if (path)
 	{
 		archiver[type].program[0] = path;
 		archiver[type].type = g_slist_append(archiver[type].type, "gzip");
 		archiver[type].glob = g_slist_append(archiver[type].glob, "*.gz");
 
-		ask[type] = xa_gzip_et_al_ask;
-		list[type] = xa_gzip_et_al_list;
-		test[type] = xa_gzip_et_al_test;
-		extract[type] = xa_gzip_et_al_extract;
-		add[type] = xa_gzip_et_al_add;
+		ask[type] = (standard ? xa_gzip_et_al_ask : xa_7zip_ask);
+		list[type] = (standard ? xa_gzip_et_al_list : xa_7zip_list);
+		test[type] = (standard ? xa_gzip_et_al_test : xa_7zip_test);
+		extract[type] = (standard ? xa_gzip_et_al_extract : xa_7zip_extract);
+		add[type] = (standard ? xa_gzip_et_al_add : xa_7zip_add);
 	}
 
 	/* LHA */
