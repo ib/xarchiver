ib/xarchiver

commit c841b7b82ec02dad60b5553d4e267ae183e9652e

Change status bar after error
Report that an error has occurred.

src/window.c
@@ -785,8 +785,13 @@ void xa_child_processed (XAChildProcess process, gboolean success, XArchive *arc
 		}
 		else
 		{
-			if (xa_main_window && !gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(prefs_window->store_output)))
-				xa_show_message_dialog(GTK_WINDOW(xa_main_window), GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, _("An error occurred!"), _("Please check the 'Store archiver output' option to see it."));
+			if (xa_main_window)
+			{
+				if (!gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(prefs_window->store_output)))
+					xa_show_message_dialog(GTK_WINDOW(xa_main_window), GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, _("An error occurred!"), _("Please check the 'Store archiver output' option to see it."));
+
+				gtk_label_set_text(GTK_LABEL(total_label), _("An error occurred!"));
+			}
 			else
 				xa_show_archive_output(NULL, archive);
 

commit 672f07461c92412317a8fc85825b5dd449d92c31

Fix bug with error output failing to appear
Since commit c841b7b82ec02dad60b5553d4e267ae183e9652e
the error output fails to appear even if configured.

Fix the bug introduced in that commit.

src/window.c
@@ -787,15 +787,13 @@ void xa_child_processed (XAChildProcess process, gboolean success, XArchive *arc
 		}
 		else
 		{
-			if (xa_main_window)
-			{
-				if (!gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(prefs_window->store_output)))
-					xa_show_message_dialog(GTK_WINDOW(xa_main_window), GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, _("An error occurred!"), _("Please check the 'Store archiver output' option to see it."));
-
-				gtk_label_set_text(GTK_LABEL(total_label), _("An error occurred!"));
-			}
+			if (xa_main_window && !gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(prefs_window->store_output)))
+				xa_show_message_dialog(GTK_WINDOW(xa_main_window), GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, _("An error occurred!"), _("Please check the 'Store archiver output' option to see it."));
 			else
 				xa_show_archive_output(NULL, archive);
+
+			if (xa_main_window)
+				gtk_label_set_text(GTK_LABEL(total_label), _("An error occurred!"));
 
 			/* in case the user has supplied a wrong password, reset it so they can try again */
 			if (archive->password && (archive->status == XARCHIVESTATUS_TEST || archive->status == XARCHIVESTATUS_SFX))