5a426aa5aea25d6e59c97349b7da0abc729d8113
Ingo Brückl <ib@wupperonline.de>
2017-05-31 10:37:11 +0200

    Improve the generic column parser
    Enable grabbing multiple columns at once.

--- src/parser.h
+++ src/parser.h
@@ -19,18 +19,26 @@
 #ifndef XARCHIVER_PARSER_H
 #define XARCHIVER_PARSER_H
 
-#define GRAB_ITEM(item, cond)                              \
+#define GRAB_ITEMS(parts, item, cond)                        \
 do                                                         \
 {                                                          \
+  unsigned int part = 1;                                     \
+                                                             \
+  do                                                         \
+  {                                                          \
   while (*line && (*line == ' ' || *line == '\t')) line++; \
-  item = line;                                             \
+  if (part == 1) item = line;                                \
   while (*line && (cond)) line++;                          \
+  }                                                          \
+  while (++part <= parts);                                   \
+                                                             \
   if (*line) *line++ = 0;                                  \
 }                                                          \
 while (0)
 
-#define NEXT_ITEM(item) GRAB_ITEM(item, *line != ' ' && *line != '\t' && *line != '\n')
-#define LAST_ITEM(item) GRAB_ITEM(item, *line != '\n')   // for filenames
+#define NEXT_ITEMS(parts, item) GRAB_ITEMS(parts, item, *line != ' ' && *line != '\t' && *line != '\n')
+#define NEXT_ITEM(item) NEXT_ITEMS(1, item)
+#define LAST_ITEM(item) GRAB_ITEMS(1, item, *line != '\n')   // for filenames
 #define SKIP_ITEM NEXT_ITEM(line)
 
 #endif
