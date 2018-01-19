5c5f89acc4a078d8828a3321841ab9c8d4538fdc
Ingo Brückl <ib@wupperonline.de>
2018-01-19 13:49:46 +0100

    Wait until drag and drop data has been set by the drop destination
    This partly fixes github issue #41, reported by mirh.

--- src/window.c
+++ src/window.c
@@ -2150,6 +2150,12 @@ void drag_data_get (GtkWidget *widget,GdkDragContext *dc,GtkSelectionData *selec
 		_destination = g_realloc(_destination, length + 1);
 		_destination[length] = 0;
 
+		if (strcmp((const char *) _destination, XDS_FILENAME) == 0)
+		{
+			g_free(_destination);
+			return;
+		}
+
 		destination = g_filename_from_uri((gchar*)_destination,NULL,NULL);
 		g_free(_destination);
 
