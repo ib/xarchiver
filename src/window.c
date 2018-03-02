ib/xarchiver

commit 836e1c5c2d3a187dec4a67048e1bfb5e66adaacf

Revise the context menu
Don't allow the open popup for a multiple selection and
don't allow renaming directory entries.

(Every compressor needs different options in order to handle the
renaming of a directory which currently is composed of extracting,
deleting and adding a directory, and a particular compressor can
even need different options for any of the three steps).

src/window.c
@@ -2565,30 +2565,20 @@ int xa_mouse_button_event(GtkWidget *widget,GdkEventButton *event,XArchive *arch
 			gtk_tree_selection_unselect_all (selection);
 			gtk_tree_selection_select_iter (selection,&iter);
 		}
-		if (selected > 1)
+
+		if (selected > 1 || entry->is_dir)
 		{
-			if (entry->is_dir)
-				gtk_widget_set_sensitive(open_popupmenu,FALSE);
-			else
-				gtk_widget_set_sensitive(open_popupmenu, archive->can_extract);
-			gtk_widget_set_sensitive(rrename,FALSE);
-			gtk_widget_set_sensitive(view,FALSE);
+			gtk_widget_set_sensitive(open_popupmenu, FALSE);
+			gtk_widget_set_sensitive(view, FALSE);
+			gtk_widget_set_sensitive(rrename, FALSE);
 		}
 		else
 		{
-			if (entry->is_dir)
-			{
-				gtk_widget_set_sensitive(view,FALSE);
-				gtk_widget_set_sensitive(open_popupmenu,FALSE);
-			}
-			else
-			{
-				gtk_widget_set_sensitive(open_popupmenu, archive->can_extract);
-				gtk_widget_set_sensitive(view, archive->can_extract);
-			}
-
+			gtk_widget_set_sensitive(open_popupmenu, archive->can_extract);
+			gtk_widget_set_sensitive(view, archive->can_extract);
 			gtk_widget_set_sensitive(rrename, can_rename(archive));
 		}
+
 		clipboard = gtk_clipboard_get(XA_CLIPBOARD);
 		clipboard_selection = gtk_clipboard_wait_for_contents(clipboard,XA_INFO_LIST);
 		if (clipboard_selection != NULL)

commit ba82218c735e14678e446c90880ad2a1fab704c6

Re-enable opening a multiple selection of entries
Allow the open popup for a multiple selection.

This fixes a regression introduced
in commit 836e1c5c2d3a187dec4a67048e1bfb5e66adaacf.

src/window.c
@@ -2541,7 +2541,7 @@ gboolean xa_mouse_button_event (GtkWidget *widget, GdkEventButton *event, XArchi
 		{
 			if (selected > 1 || entry->is_dir)
 			{
-				gtk_widget_set_sensitive(open_popupmenu, FALSE);
+				gtk_widget_set_sensitive(open_popupmenu, !entry->is_dir && archive->can_extract);
 				gtk_widget_set_sensitive(view, FALSE);
 				gtk_widget_set_sensitive(rrename, FALSE);
 			}