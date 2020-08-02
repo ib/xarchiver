9b1e07f6421294e44b98b89cd05da22176b6544d
Ingo Brückl <ib@wupperonline.de>
2020-08-02 13:22:19 +0200

    Remove variable iso_date
    It is no longer needed.
    
    Additionally, reposition the comments on the date formats.

--- src/rar.c
+++ src/rar.c
@@ -260,7 +260,7 @@ static void xa_rar5_parse_output (gchar *line, XArchive *archive)
 	gpointer item[7];
 	unsigned short int i = 0;
 	unsigned int linesize, n, a;
-	gboolean encrypted = FALSE, dir = FALSE, iso_date;
+	gboolean encrypted = FALSE, dir = FALSE;
 	static gchar *filename, *end;
 
 	if (last_line)
@@ -363,9 +363,9 @@ static void xa_rar5_parse_output (gchar *line, XArchive *archive)
 	a = n;
 	for(; n < linesize && line[n] != ' '; n++);
 	line[n] = '\0';
-	item[i] = line + a;
-	iso_date = (strlen(item[i]) == 10);
-	if (!iso_date) item[i] = date_DD_MM_YY(item[i]);
+	item[i] = line + a;          // date is YYYY-MM-DD since v5.30
+	if (strlen(item[i]) != 10)   // and was DD-MM-YY before
+		item[i] = date_DD_MM_YY(item[i]);
 	i++;
 	n++;
 
@@ -387,8 +387,8 @@ static void xa_rar5_parse_output (gchar *line, XArchive *archive)
 
 	/* FileName */
 	line[linesize - 1] = '\0';
-	filename = g_strdup(line + n + 2);                  // date is YYYY-MM-DD since v5.30
-	                                                    // and was DD-MM-YY before
+	filename = g_strdup(line + n + 2);
+
 	/* Strip trailing whitespace */
 	end = filename + strlen(filename) - 1;
 	while(end >= filename && *end == ' ') end--;
