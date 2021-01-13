ib/xarchiver

commit 785bcb7b86648d4dc137424f3b8884ebdf9782e7

Rebuild child related code
After having spawned a child asynchronously there are three processes
to be watched:

* the child's standard output
* the child's standard error
* the child's exit status

Post-processing (i.e. user interface actions) must not start until all
of the three processes have finished. So move all post-processing code
to the post-processing function (currently named xa_watch_child()).
Furthermore, the post-processing function must not assume that it is
called last, so add a new function xa_process_exit() and move all exit
related code to it.

Each process now reports to the post-processing function when it is
done. This will be tracked by new XArchive member child_ref.

This rebuild is first step and a rather dirty approach that aims for the
goals to not break executability and to make minimal, easy recognizable
changes. It will be enhanced one by one.

src/archive.c
@@ -79,32 +79,8 @@ static gboolean xa_process_output (GIOChannel *ioc, GIOCondition cond, gpointer 
 		g_io_channel_shutdown (ioc,TRUE,NULL);
 		g_io_channel_unref (ioc);
 
-		if (archive->parse_output)
-		{
-			if (archive->has_comment && archive->status == XA_ARCHIVESTATUS_OPEN && gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(prefs_window->check_show_comment)))
-				xa_show_archive_comment (NULL,NULL);
+		xa_watch_child(1, TRUE, archive);
 
-			xa_update_window_with_archive_entries (archive,NULL);
-			gtk_tree_view_set_model (GTK_TREE_VIEW(archive->treeview),archive->model);
-			g_object_unref (archive->model);
-
-			gtk_widget_set_sensitive(comment_menu, archive->has_comment);
-			gtk_widget_set_sensitive(password_entry_menu, archive->has_passwd);
-			gtk_widget_set_sensitive(listing,TRUE);
-
-			if (GTK_IS_TREE_VIEW(archive->treeview))
-				gtk_widget_grab_focus (GTK_WIDGET(archive->treeview));
-
-			xa_set_statusbar_message_for_displayed_rows(archive);
-
-			if (archive->status == XA_ARCHIVESTATUS_TEST)
-			{
-				archive->create_image = FALSE;
-				xa_show_cmd_line_output (NULL,archive);
-			}
-			if (archive->status == XA_ARCHIVESTATUS_OPEN)
-				xa_set_button_state (1,1,1,1,archive->can_add,archive->can_extract,archive->can_sfx,archive->can_test,archive->has_passwd,1);
-		}
 		return FALSE;
 	}
 	return TRUE;
@@ -137,9 +113,30 @@ static gboolean xa_dump_child_error_messages (GIOChannel *ioc, GIOCondition cond
 	done:
 		g_io_channel_shutdown (ioc, TRUE, NULL);
 		g_io_channel_unref (ioc);
+
+		xa_watch_child(2, TRUE, archive);
+
 		return FALSE;
 	}
 	return TRUE;
+}
+
+static void xa_process_exit (GPid pid, gint status, XArchive *archive)
+{
+	gboolean result;
+
+	if (WIFEXITED(status))
+	{
+		result = (WEXITSTATUS(status) == 0);
+
+		if ((WEXITSTATUS(status) == 1 && archive->type == XARCHIVETYPE_ZIP) ||
+		    (WEXITSTATUS(status) == 6 && archive->type == XARCHIVETYPE_ARJ) ||
+		    (WEXITSTATUS(status) == 1 && is_tar_compressed(archive->type)))
+			result = TRUE;
+
+		g_spawn_close_pid(pid);
+		xa_watch_child(0, result, archive);
+	}
 }
 
 static void xa_delete_temp_directory (XArchive *archive)
@@ -339,6 +336,8 @@ void xa_spawn_async_process (XArchive *archive, gchar *command)
 	}
 	g_strfreev (argv);
 
+	archive->child_ref = 3;
+
 	if (archive->status == XA_ARCHIVESTATUS_OPEN)
 		archive->pb_source = g_timeout_add (350,(GSourceFunc)xa_flash_led_indicator,archive);
 
@@ -356,7 +355,7 @@ void xa_spawn_async_process (XArchive *archive, gchar *command)
 	g_io_add_watch(ioc, G_IO_IN | G_IO_PRI | G_IO_ERR | G_IO_HUP | G_IO_NVAL, xa_process_output, archive);
 
 	if (archive->parse_output)
-		g_child_watch_add_full (G_PRIORITY_LOW,archive->child_pid, (GChildWatchFunc)xa_watch_child,archive,NULL);
+		g_child_watch_add_full(G_PRIORITY_LOW, archive->child_pid, (GChildWatchFunc) xa_process_exit, archive, NULL);
 
 	err_ioc = g_io_channel_unix_new (archive->error_fd);
 	g_io_channel_set_encoding (err_ioc,locale,NULL);
@@ -474,7 +473,6 @@ gboolean xa_run_command (XArchive *archive,GSList *commands)
 	gboolean result = TRUE;
 	GSList *_commands = commands;
 
-	archive->parse_output = NULL;
 	if (xa_main_window)
 	{
 		gtk_widget_set_sensitive (Stop_button,TRUE);
@@ -512,9 +510,12 @@ gboolean xa_run_command (XArchive *archive,GSList *commands)
 				break;
 			}
 		}
+
+		xa_watch_child(0, result, archive);
+
 		_commands = _commands->next;
 	}
-	xa_watch_child (archive->child_pid, status, archive);
+
 	if (xa_main_window)
 		xa_set_button_state(1, 1, 1, 1, archive->can_add, archive->can_extract, archive->can_sfx, archive->can_test, archive->has_passwd, 1);
 

src/archive.h
@@ -157,6 +157,7 @@ struct _XArchive
 	/* child process */
 	gchar *working_dir;
 	GPid child_pid;
+	guint child_ref;
 	gint output_fd;
 	gint error_fd;
 	GSList *error_output;

src/window.c
@@ -23,8 +23,6 @@
 #include <gdk/gdkkeysyms.h>
 #include <glib/gprintf.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include "window.h"
 #include "add_dialog.h"
 #include "extract_dialog.h"
@@ -758,19 +756,17 @@ static void xa_clipboard_cut_copy_operation (XArchive *archive, XAClipboardMode 
 	}
 }
 
-void xa_watch_child (GPid pid,gint status,XArchive *archive)
+void xa_watch_child (int process, gboolean success, XArchive *archive)
 {
-	archive->child_pid = archive->pb_source = 0;
-	if (WIFEXITED (status))
+	archive->child_ref--;
+
+	if (process == 0)
+		archive->child_pid = 0;
+
+	if (!success)
 	{
-		if (WEXITSTATUS (status))
-		{
 			if (xa_main_window == NULL)
 				goto error;
-			if ((WEXITSTATUS (status) == 1 && archive->type == XARCHIVETYPE_ZIP) ||
-				(WEXITSTATUS (status) == 6 && archive->type == XARCHIVETYPE_ARJ) ||
-				(WEXITSTATUS (status) == 1 && is_tar_compressed(archive->type)))
-				goto there;
 			if ( ! gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(prefs_window->store_output)))
 			{
 				xa_show_message_dialog(GTK_WINDOW(xa_main_window),GTK_DIALOG_MODAL,GTK_MESSAGE_ERROR,GTK_BUTTONS_OK,_("An error occurred!"),_("Please check the 'Store archiver output' option to see it."));
@@ -792,16 +788,45 @@ void xa_watch_child (GPid pid,gint status,XArchive *archive)
 				archive->status = XA_ARCHIVESTATUS_ERROR;
 				return;
 			}
-		}
 	}
-there:
 	if (xa_main_window)
 	{
+		if (archive->parse_output && process == 1)
+		{
+			archive->parse_output = NULL;
+
+			if (archive->has_comment && archive->status == XA_ARCHIVESTATUS_OPEN && gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(prefs_window->check_show_comment)))
+				xa_show_archive_comment (NULL,NULL);
+
+			xa_update_window_with_archive_entries (archive,NULL);
+			gtk_tree_view_set_model (GTK_TREE_VIEW(archive->treeview),archive->model);
+			g_object_unref (archive->model);
+
+			gtk_widget_set_sensitive(comment_menu, archive->has_comment);
+			gtk_widget_set_sensitive(password_entry_menu, archive->has_passwd);
+			gtk_widget_set_sensitive(listing,TRUE);
+
+			if (GTK_IS_TREE_VIEW(archive->treeview))
+				gtk_widget_grab_focus (GTK_WIDGET(archive->treeview));
+
+			xa_set_statusbar_message_for_displayed_rows(archive);
+
+			if (archive->status == XA_ARCHIVESTATUS_TEST)
+			{
+				archive->create_image = FALSE;
+				xa_show_cmd_line_output (NULL,archive);
+			}
+			if (archive->status == XA_ARCHIVESTATUS_OPEN)
+				xa_set_button_state (1,1,1,1,archive->can_add,archive->can_extract,archive->can_sfx,archive->can_test,archive->has_passwd,1);
+		}
+		if (archive->child_ref == 0)
+		{
 		xa_set_button_state (1,1,1,1,archive->can_add,archive->can_extract,0,archive->can_test,archive->has_passwd,1);
-		archive->child_pid = archive->pb_source = 0;
+		archive->pb_source = 0;
 		gtk_widget_set_sensitive(Stop_button,FALSE);
 		gtk_label_set_text(GTK_LABEL(total_label),"");
 		archive->status = XA_ARCHIVESTATUS_IDLE;
+		}
 	}
 }
 

src/window.h
@@ -72,6 +72,6 @@ void xa_treeview_row_activated(GtkTreeView *, GtkTreePath *, GtkTreeViewColumn *
 void xa_unsort(GtkMenuItem *, gpointer);
 void xa_update_window_with_archive_entries(XArchive *, XEntry *);
 void xa_view_from_popupmenu(GtkMenuItem *, gpointer);
-void xa_watch_child(GPid, gint, XArchive *);
+void xa_watch_child(int, gboolean, XArchive *);
 
 #endif

commit a03bb333089679d3b77d67b28f0a62d465a29cfd

Fix the broken cancellation of an operation
Allow for a possible process termination by a kill signal and handle it.

This has been broken in 785bcb7b86648d4dc137424f3b8884ebdf9782e7 with
the implementation of xa_process_exit().

src/archive.c
@@ -133,10 +133,14 @@ static void xa_process_exit (GPid pid, gint status, XArchive *archive)
 	{
 		if ((archive->status == XARCHIVESTATUS_RELOAD) && !g_file_test(archive->path[0], G_FILE_TEST_EXISTS))
 			status = 0;
-
-		g_spawn_close_pid(pid);
-		xa_child_processed(XA_CHILD_EXIT, WEXITSTATUS(status), archive);
+		else
+			status = WEXITSTATUS(status);
 	}
+	else
+		status = 1;
+
+	g_spawn_close_pid(pid);
+	xa_child_processed(XA_CHILD_EXIT, status, archive);
 }
 
 static void xa_delete_working_directory (XArchive *archive)