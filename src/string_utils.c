5b859cbb8ed022d068422819e19964b342a4e49d
Ingo Brückl <ib@wupperonline.de>
2021-01-19 13:24:20 +0100

    Fix segmentation fault
    If a directory is to be compressed that cannot be opened (because
    access is denied, for example), a segmentation fault will result.
    
    Patch by kittennbfive, github issue #114.

--- src/string_utils.c
+++ src/string_utils.c
@@ -314,8 +314,8 @@ void xa_recurse_local_directory (gchar *path, GSList **list, gboolean full_path,
 				basename = g_path_get_basename(path);
 				*list = g_slist_prepend(*list, basename);
 			}
-			return;
 		}
+		return;
 	}
 
 	while ((dirlist = readdir(dir)))
