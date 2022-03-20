24817c032e1431ceb23e5ec3daa61855400c3dd8
Ingo Brückl <ib@wupperonline.de>
2022-03-20 16:52:59 +0100

    Improve drag operation and feedback in xa_dir_sidebar_drag_motion()
    Allow and illustrate copy dropping on a TreeView row only. Change the
    drag icon to illustrate that dropping is not possible in other parts
    of dir_sidebar.
    
    Also, clear the context data "tree_path" in other parts of dir_sidebar
    so that dropping will not occur on the last touched TreeView row.

--- src/interface.c
+++ src/interface.c
@@ -330,10 +330,15 @@ static gboolean xa_dir_sidebar_drag_motion (GtkWidget *widget, GdkDragContext *c
 		}
 
 		g_object_set_data(G_OBJECT(context), "tree_path", path);
+		gdk_drag_status(context, GDK_ACTION_COPY, time);
+	}
+	else
+	{
+		g_object_set_data(G_OBJECT(context), "tree_path", NULL);
+		gdk_drag_status(context, 0, time);
 	}
 
 	gtk_tree_view_set_drag_dest_row(GTK_TREE_VIEW(widget), path, GTK_TREE_VIEW_DROP_INTO_OR_BEFORE);
-	gdk_drag_status(context, GDK_ACTION_COPY, time);
 
 	return TRUE;
 }
